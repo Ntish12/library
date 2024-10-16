b048e19d26205aa32b798041696f81e27bbe1e73
Ntish12 <157442528+Ntish12@users.noreply.github.com>
2024-10-16 19:44:27 -0400

    Add files via upload

---
+++ Members.sql
@@ -0,0 +1,11 @@
+INSERT INTO Members (first_name, last_name, email, membership_date)
+values ('Ntish', 'Kumar', 'ntish@gmail.com', '2024-05-15'),
+('Khushmeet', 'Singh', 'khusmeet545@gmail.com', '2024-02-13'),
+('Akash', 'singh', 'akash@gmail.com', '2024-09-15'),
+('Om', 'Shivam', 'om2005@gmail.com', '2024-01-15'),
+('Anmol', 'Singh', 'anmol@gmail.com', '2024-09-15'),
+('Manjit', 'Maan', 'Maan2005@gmail.com', '2024-10-15'),
+('Prabh', 'Boparai', 'Boparai07@gmail.com', '2024-08-15'),
+('Daljit', 'Singh', 'daljit@gmail.com', '2024-07-15'),
+('Harpreet', 'Singh', 'harpreet@gmail.com', '2024-06-15'),
+('Gurveer', 'Singh', 'gurveer@gmail.com', '2024-05-15');
---
+++ borrowing_record.sql
@@ -0,0 +1,6 @@
+INSERT INTO borrowing_records (memberID, bookID, borrow_date, return_date)
+VALUES(1, 5, '2024-09-01', '2024-09-21'), 
+(3, 2, '2024-09-02', NULL), 
+(2, 9, '2024-09-16', '2024-09-06'), 
+(4, 4, '2024-09-07', NULL), 
+(5, 7, '2024-09-29', '2024-10-17');
